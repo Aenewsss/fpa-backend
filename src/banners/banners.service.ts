5d364ac727f90627f6b82a3d83c42a343843ecfd
Aenewsss <aenamartinelli@gmail.com>
2025-08-24 18:09:08 -0300

    fix: add temp value to order while updating

--- src/banners/banners.service.ts
+++ src/banners/banners.service.ts
@@ -57,27 +57,35 @@ export class BannersService {
 
         const isMovingDown = newOrder > currentOrder;
 
-        // Atualiza os banners entre a posição atual e a nova
-        await this.prisma.banner.updateMany({
-            where: {
-                removed: false,
-                id: { not: id },
-                order: {
-                    gte: isMovingDown ? Number(currentOrder) + 1 : Number(newOrder),
-                    lte: isMovingDown ? Number(newOrder) : Number(currentOrder) - 1,
-                },
-            },
-            data: {
-                order: {
-                    increment: isMovingDown ? -1 : 1,
-                },
-            },
-        });
+        return this.prisma.$transaction(async (tx) => {
+            // 1. Remove temporariamente o banner da ordem atual
+            await tx.banner.update({
+                where: { id },
+                data: { order: -1 }, // valor temporário
+            });
 
-        // Atualiza o banner desejado
-        return this.prisma.banner.update({
-            where: { id },
-            data: { order: Number(newOrder) },
+            // 2. Atualiza os banners entre as posições
+            await tx.banner.updateMany({
+                where: {
+                    removed: false,
+                    id: { not: id },
+                    order: {
+                        gte: isMovingDown ? Number(currentOrder) + 1 : Number(newOrder),
+                        lte: isMovingDown ? Number(newOrder) : Number(currentOrder) - 1,
+                    },
+                },
+                data: {
+                    order: {
+                        increment: isMovingDown ? -1 : 1,
+                    },
+                },
+            });
+
+            // 3. Atualiza o banner para a nova posição
+            return tx.banner.update({
+                where: { id },
+                data: { order: Number(newOrder) },
+            });
         });
     }
 }
